3b99066a9e026f5f580b58fd5b3327fccc42d366
KimuKunTai <97505218+KimuKunTai@users.noreply.github.com>
2025-07-17 13:37:50 +0900

    Add files via upload

---
+++ src/constants/theme.ts
@@ -0,0 +1,14 @@
+export const THEME_COLOR = {
+    clay: {
+        primary: '#000000',
+        secondary: '#f9a410',
+    },
+    lawn: {
+        primary: '#205e26',
+        secondary: '#e56520',
+    },
+    hard: {
+        primary: '#501b85',
+        secondary: '#50851b',
+    },
+};
