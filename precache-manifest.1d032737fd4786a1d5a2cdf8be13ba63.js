5a8792fea346c4cc93aecefeb25d121e3cb772a5
KimuKunTai <97505218+KimuKunTai@users.noreply.github.com>
2025-07-20 23:22:01 +0900

    Add files via upload

---
+++ precache-manifest.1d032737fd4786a1d5a2cdf8be13ba63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06de93bb96c0a60fc0d36f581faf1588",
+    "url": "/hi-tennis/index.html"
+  },
+  {
+    "revision": "6498b484dbf9be743cb8",
+    "url": "/hi-tennis/static/css/2.a581e323.chunk.css"
+  },
+  {
+    "revision": "bd3e395fd9b1475fbe0e",
+    "url": "/hi-tennis/static/css/main.4c4318a8.chunk.css"
+  },
+  {
+    "revision": "6498b484dbf9be743cb8",
+    "url": "/hi-tennis/static/js/2.1760deda.chunk.js"
+  },
+  {
+    "revision": "8389a2429ff64b5b0b88d51361993381",
+    "url": "/hi-tennis/static/js/2.1760deda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3e395fd9b1475fbe0e",
+    "url": "/hi-tennis/static/js/main.6e7b2152.chunk.js"
+  },
+  {
+    "revision": "d8eb09b0fa05be747a35",
+    "url": "/hi-tennis/static/js/runtime-main.f593f710.js"
+  }
+]);
